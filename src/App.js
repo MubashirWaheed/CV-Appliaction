7dcf9a7e0ecdd43d4c32c5754283b0e677634904
MubashirWaheed <mubashir3080@gmail.com>
2021-10-26 03:47:38 +0500

    restructured the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import GeneralInfo from './components/GeneralInfo';
-
 function App() {
   return (
     <div className="App">
-      <GeneralInfo title="mubashir"/>
+      {/* <GeneralInfo title="mubashir"/> */}
+      <div className="background">
+        <form typeof="submit">
+          <div className="background-card">
+            <h1>Cv Generator</h1>
+            <GeneralInfo />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -16,14 +16,45 @@ class GeneralInfo extends Component{
     }
 
     render(){
-        return(
-            <div>
-                <p>{this.props.title}</p>
-                <input className="input-text" onChange={this.handleChange} type="text"/>
-                <FontAwesomeIcon icon={faEdit}/>
+        return( 
+            <div className="main">
+                <div className="row">
+                    <div className="flex name">
+                        <label htmlFor="name">Name *</label>
+                        <input type="text" name="name" id="name" required />
+                    </div>
+                    <div className="flex email ">
+                        <label htmlFor="name">Email *</label>
+                        <input type="text" name="email" id="email-input" required/>
+                    </div>
+                </div>
+                
+
             </div>
+
+
+                
         )            
     }
 
 }
+
+
+// <div className="background">
+                // <form type="submit">
+                    // <div className="background-card">
+                        // <h1>Cv Generator</h1>
+                        {/* <div> */}
+                            {/* <label>First Name *</label><input type="text" required/>     */}
+                            {/* <label>Last Name *</label><input type="text" name="" id="" required/>                      */}
+                            {/* <label htmlFor="email">Email *</label><input className="input-text" onChange={this.handleChange} type="email" id="email"/> */}
+                            {/* <label htmlFor="">Phone *</label><input type="number" /> */}
+                        {/* </div> */}
+                        {/* <p>{this.props.title}</p> */}
+                        
+            //             <FontAwesomeIcon icon={faEdit}/>
+            //             <button type="submit">Submit</button>
+            //         </div>
+            //     </form>
+            // </div>
 export default GeneralInfo
