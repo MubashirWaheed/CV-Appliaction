e25813c948ef5e778ec8e53c55ab92080ca2cc29
MubashirWaheed <mubashir3080@gmail.com>
2021-10-26 03:51:22 +0500

    removed previous code

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -28,33 +28,11 @@ class GeneralInfo extends Component{
                         <input type="text" name="email" id="email-input" required/>
                     </div>
                 </div>
-                
-
             </div>
-
-
-                
+  
         )            
     }
 
 }
 
-
-// <div className="background">
-                // <form type="submit">
-                    // <div className="background-card">
-                        // <h1>Cv Generator</h1>
-                        {/* <div> */}
-                            {/* <label>First Name *</label><input type="text" required/>     */}
-                            {/* <label>Last Name *</label><input type="text" name="" id="" required/>                      */}
-                            {/* <label htmlFor="email">Email *</label><input className="input-text" onChange={this.handleChange} type="email" id="email"/> */}
-                            {/* <label htmlFor="">Phone *</label><input type="number" /> */}
-                        {/* </div> */}
-                        {/* <p>{this.props.title}</p> */}
-                        
-            //             <FontAwesomeIcon icon={faEdit}/>
-            //             <button type="submit">Submit</button>
-            //         </div>
-            //     </form>
-            // </div>
 export default GeneralInfo
